4c509e0d9b0ee58557dc2ab5dfccb1ce4cc1788b
RobertoMann77 <120669323+RobertoMann77@users.noreply.github.com>
2023-12-09 07:45:27 +0100

    Update day09.py

--- 2023/day09/day09.py
+++ 2023/day09/day09.py
@@ -9,57 +9,42 @@ for l,line in enumerate(data):
 	for c,ccc in enumerate(line):
 		data[l][c] = int(data[l][c])
 
+
+def zero_check(a):
+	check = 0
+	for j in a:
+		if j != 0: check =1
+	if check == 1: 
+		return True
+	else:
+		return False
+
 #part 1
 sum1 = 0
+sum2 = 0
 for line in data:
 	new = [line]
-	not_zero = True
-	while not_zero == True:
-		n = []
+	while zero_check(new[-1]) == True:
+		new_line = []
 		for i in range(len(new[-1])-1):
-			n.append(new[-1][i+1]-new[-1][i])
-		new.append(n)
+			new_line.append(new[-1][i+1]-new[-1][i])
+		new.append(new_line)
 
-		check = 0
-		for j in new[-1]:
-			if j != 0: check =1
-		if check == 1: 
-			not_zero = True
-		else:
-			not_zero = False
+	new1 = new
 
+	#part 1
 	new[-1].append(0)
 	for i in range(len(new)-2,-1,-1):
 		new[i].append(new[i][-1]+new[i+1][-1])
 	sum1 += new[0][-1]
-print('Answer 1:', sum1)
-		
-#part 2
-sum2 = 0
-for line in data:
-	new = [line]
-	test = True
-	while test == True:
-		n = []
-		for i in range(len(new[-1])-1):
-			n.append(new[-1][i+1]-new[-1][i])
-		new.append(n)
 
-		check = 0
-		for j in new[-1]:
-			if j != 0: check =1
-		if check == 1: 
-			test = True
-		else:
-			test = False
-
-
+	#part 2
 	new[-1] = [0] + new[-1]
 	for i in range(len(new)-2,-1,-1):
 		new[i] = [new[i][0] - new[i+1][0]] + new[i]
-
 	sum2 += new[0][0]
 
+print('Answer 1:', sum1)
 print('Answer 2:', sum2)
 
 
