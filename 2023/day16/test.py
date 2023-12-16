1e36dfdee9396d5fa07966f08c42d36393c412c5
RobertoMann77 <robertmachovec@gmx.at>
2023-12-16 15:08:42 +0100

    Update test.py

--- 2023/day16/test.py
+++ 2023/day16/test.py
@@ -1,43 +1,59 @@
-def count_tiles(p):
-	hist = [p]	
-	curr = [p]
+def count_tiles(start):
+	hist = [start]	
+	curr = [start]
 	tiles = set()
-	while curr:
-		posdir = curr.pop(0)
-		N = []				
-		d = posdir[1]
-		#	   n  e  s  w
-		dc = [ 0, 1, 0,-1]
-		dr = [-1, 0, 1, 0]
-		
-		nr = posdir[0][0] + dr[d]
-		nc = posdir[0][1] + dc[d]
+	while curr:	
+		(r,c,dr,dc) = curr.pop(0)
 
-		if  0 <= nr < rows and 0 <= nc < cols:
-			m = M[nr][nc]
-			if m == '.': N.append(((nr,nc),d))
+		r += dr
+		c += dc
 
-			elif m == '|':	
-				if d == 2 or d == 0: N.append(((nr,nc),d))
-				elif d == 1 or d == 3:
-					N.append(((nr,nc),0))
-					N.append(((nr,nc),2))
+		if  0 <= r  < rows and 0 <= c  < cols:
+			ch = M[r][c]
+			if ch == '.':
+				if (r,c,dr,dc) not in hist:
+					curr.append((r,c,dr,dc))
+					hist.append((r,c,dr,dc))
+
+			elif ch == '|':	
+				if dc == 0: 
+					if (r,c,dr,dc) not in hist:
+						curr.append((r,c,dr,dc))
+						hist.append((r,c,dr,dc))
+
+				elif dr == 0:
+					if (r,c,-1,0) not in hist:
+						curr.append((r,c,-1,0))
+						hist.append((r,c,-1,0))
+
+					if (r,c,1,0) not in hist:
+						curr.append((r,c,1,0))
+						hist.append((r,c,1,0))
 				
-			elif m == '-':	
-				if d == 1 or d == 3: N.append(((nr,nc),d))
-				elif d == 0 or d == 2:
-					N.append(((nr,nc),1))
-					N.append(((nr,nc),3))
+			elif ch == '-':	
+				if dr == 0:
+					if (r,c,dr,dc) not in hist:
+						curr.append((r,c,dr,dc))
+						hist.append((r,c,dr,dc))
 
-			elif m == '\\':	N.append(((nr,nc),{1 : 2, 3 : 0, 0 : 3, 2 : 1}[d]))
-			elif m == '/': N.append(((nr,nc),{1 : 0, 3 : 2, 0 : 1, 2 : 3}[d]))
+				elif dc == 0:
+					if (r,c,0,1) not in hist:
+						curr.append((r,c,0,1))
+						hist.append((r,c,0,1))
 
-			tiles.add((nr,nc))
-		
-		for n in N:
-			if n not in hist:
-				curr.append(n)
-			hist.append(n)
+					if (r,c,0,-1) not in hist:
+						curr.append((r,c,0,-1))
+						hist.append((r,c,0,-1))
+
+			elif ch == '\\' and (r,c,dc,dr) not in hist:
+				curr.append((r,c,dc,dr))
+				hist.append((r,c,dc,dr))
+
+			elif ch == '/' and (r,c,-dc,-dr) not in hist: 
+				curr.append((r,c,-dc,-dr))
+				hist.append((r,c,-dc,-dr))
+			tiles.add((r,c))
+
 	return len(tiles)
 
 M = open('16.in').read().splitlines()
@@ -45,25 +61,24 @@ M = open('16.in').read().splitlines()
 rows = len(M)
 cols = len(M[0])
 
+print('Answer 1:',count_tiles((0, -1, 0, 1)))
 
-print('Answer 1:',count_tiles(((0, -1), 1)))
 
-
-'''record = []
-
-for c in range(cols):
-	record.append(count_tiles(((-1, c), 2)))
+record = []
 
 for c in range(cols):
-	record.append(count_tiles(((rows, c), 0)))
+	record.append(count_tiles((-1, c, 1,0)))
+
+for c in range(cols):
+	record.append(count_tiles((rows, c, -1,0)))
 
 for r in range(cols):
-	record.append(count_tiles(((r, -1), 1)))
+	record.append(count_tiles((r, -1, 0,1)))
 
 for r in range(cols):
-	record.append(count_tiles(((r, rows), 3)))
+	record.append(count_tiles((r, rows, 0,-1)))
 	
-print('Answer 2:',max(record))'''
+print('Answer 2:',max(record))
 
 
 
