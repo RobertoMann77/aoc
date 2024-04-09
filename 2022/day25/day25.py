40679a1d56b379e12d29dc517359b228c767798d
RobertoMann77 <120669323+RobertoMann77@users.noreply.github.com>
2024-04-09 11:48:47 +0200

    2022 day25

--- 2022/day25/day25.py
+++ 2022/day25/day25.py
@@ -1,4 +1,4 @@
-data = [i.strip() for i in open('t.in')]
+data = [i.strip() for i in open('25.in')]
 
 trans = {'1' : 1,
          '2' : 2,
@@ -6,24 +6,39 @@ trans = {'1' : 1,
          '-' : -1,
          '=' : -2}
 
+def calc(s):
+    z = 0
+    for j in range(1, len(s) + 1):
+        z += trans[s[-j]] * 5**(j-1)
+    return z
+
 def back(z):
     for a, b in trans.items():
         if b == z:
             return a
 erg = 0
 for line in data:
-    for j in range(1, len(line) + 1):
-        erg += trans[line[-j]] * 5**(j-1)
-j(erg)
+    erg += calc(line)    
 
-c = -1
+c = 0
 y = 1
 while y < erg:
     y *= 5
     c += 1
 
-j(c)
+digit = ['=', '-', '0', '1', '2']
+ans = []
+for k in range(c):
+    ans += '='
 
-ans = ''
+z = ''
+for t in range(c):
+    n = c - t - 1
+    ans[t] = digit[(erg - calc(ans))//5**n]
+print(ans)
+print(calc(ans))
 
-j(erg//(5**c))
+g = ''
+for r in ans:
+    g += r
+print(g)
